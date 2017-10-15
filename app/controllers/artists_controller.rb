6d8dc758fb5827e8f0e58117aec9046fd64b3f0a
SanderNiesten <sanderniesten@live.nl>
2017-10-15 14:51:22 +0200

    Modifief Artist controller to create, added db schema

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,17 @@
 class ArtistsController < ApplicationController
 
-def new
-end
+  def new
+  end
 
-def create
-  render plain: params[:artist].inspect
-end
+  def create
+    @artist = Artist.new(article_params)
 
+    @artist.save
+    redirect_to @artist
+  end
+
+  private
+  def article_params
+    params.require(:artist).permit(:name)
+  end
 end
