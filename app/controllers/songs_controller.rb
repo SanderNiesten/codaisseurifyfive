8c5759800ef8fc0d6e634d196ec2abf0e127ecee
SanderNiesten <sanderniesten@live.nl>
2017-10-15 16:31:38 +0200

    Add Destroy in Songs controller, change view _song.html.erb

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,13 @@ class SongsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
+  def destroy
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.find(params[:id])
+    @song.destroy
+    redirect_to artist_path(@artist)
+  end
+
   private
   def song_params
       params.require(:song).permit(:name)
