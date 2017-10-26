86030cc583b6205b3f0a5587b3f1abbace1606c2
SanderNiesten <sanderniesten@live.nl>
2017-10-26 17:31:50 +0200

    Add new api/songs controllers

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,3 +1,11 @@
-function createSong(name) {
-
+function destroySong(songId) {
+  $.ajax({
+    type: "DELETE",
+    url: "/api/artists/songs/" + songId,
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    $('tr[song-id="'+songId+'"]').remove();
+  });
 }
--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,1 +1,36 @@
 skip_before_action :verify_authenticity_token
+
+before_action :artist_set
+
+  def new
+    @song = @artist.songs.build
+  end
+
+  def create
+    @song = @artist.songs.create(song_params)
+    if @song.save
+      render status: 200, json: @song
+    else
+      render status: 422, json: {
+        errors: @song.errors
+      }
+    end
+  end
+
+  def destroy
+    @song = @artist.songs.find(params[:id])
+    @song.destroy
+    render status: 200, json: {
+      message: "Song deleted"
+    }.to_json
+  end
+
+  private
+
+  def artist_set
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def song_params
+    params.require(:song).permit(:id, :name)
+  end
